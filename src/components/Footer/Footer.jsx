64ac295dc9458528a176cf533ad4f89c9f45fc08
seohyunchoiii <fooodie@naver.com>
2024-04-30 22:09:17 +0900

    Add Footer Forms Sections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,7 @@ import { HiOutlineMailOpen } from "react-icons/hi";
 import { AiFillGithub, AiFillLinkedin, AiOutlineArrowUp } from "react-icons/ai";
 import { BsFacebook, BsSlack } from "react-icons/bs";
 import { FiMail, FiPhoneCall } from "react-icons/fi";
+import COLORS from '../../common/colors';
 
 const Container = styled.div`
   margin-top: 2rem;
@@ -14,12 +15,17 @@ const Container = styled.div`
   width: 80%;
   max-width: 1280px;
   margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
   @media screen and (max-width: 840px) {
     width: 90%;
   }
   `
 const Profile = styled.div``
 const Form = styled.div``
+const Forms = styled.div``
+const Button = styled.button``
+const Name = styled.div``
 const Header = styled.h1``
 const Address = styled.h1`
   padding: 1rem 0;
@@ -58,9 +64,45 @@ const Mail = styled.div`
     }
   }
 `
-const Profiles = styled.h1``
-const Icons = styled.div``
-const ArrowUp = styled.div``
+const Profiles = styled.h1`
+  font-size: 1.2rem;
+  padding: 1rem 0;
+`
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  span{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #000;
+    width: 2rem;
+    height: 2rem;
+    margin-right: 0.5rem;
+    border-radius: 50px;
+
+    &:hover{
+      background-color: orange;
+    }
+
+    a{
+      margin-top: 0.2rem;
+      color: #fff;
+    }
+  }
+`
+const ArrowUp = styled.div`
+  width: 2rem;
+  height: 2rem;
+  background-color: ${COLORS.mint};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 1.3rem;
+  font-weight: 700;
+  margin-top: 2rem;
+`
 
 
 const Footer = () => {
@@ -91,8 +133,23 @@ const Footer = () => {
         <ArrowUp><AiOutlineArrowUp/></ArrowUp>
       </Profile>
         
-
-      <Form></Form>
+      <Form>
+        <Forms>
+          <Name>
+            <CgProfile/>
+            <input type="text" placeholder='Fullname...'/>
+          </Name>
+          <Name>
+            <MdAlternateEmail/>
+            <input type="email" placeholder='Email...'/>
+          </Name>
+          <Name>
+            <FiMail/>
+            <textarea cols="30" rows="10" placeholder='Message...'/>
+          </Name>
+          <Button>Submit</Button>
+        </Forms>        
+      </Form>
     </Container>
   )
 }
